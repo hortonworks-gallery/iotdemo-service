1d1965ea31413f56de92ab8f02c8ec18eb6accd6
Ali Bajwa <abajwa-hw@users.noreply.github.com>
2017-04-25 16:42:54 -0700

    Update setup.sh
    If storm lib dir doesn't contain 2.6.2 log4j jars, replace 2.8 jars with 2.6.2

--- package/scripts/setup.sh
+++ package/scripts/setup.sh
@@ -160,13 +160,13 @@ EOF
 fi
 
 
-#if storm located on same node and its lib dir doesn't contain 2.6.2 log4j jars, replace 2.1 jars with 2.6
-#if [ -d /usr/hd*/2.*/storm/lib/ ] && [ $(ls -la /usr/hd*/2.*/storm/lib/log4j*2.6.2.jar | wc -l) != 3 ]; then
-#  echo "Updating storm jar in storm lib dir..."
-#  mkdir ${demo_root}/oldjars
-#  mv /usr/hd*/2.*/storm/lib/log4j*-2.1.jar ${demo_root}/oldjars
-#  cp ${demo_root}/hdp/reference-apps/iot-trucking-app/trucking-data-simulator/target/log4j*-2.6.2.jar /usr/hd*/2.*/storm/lib/
-#fi
+#if storm located on same node and its lib dir doesn't contain 2.6.2 log4j jars, replace 2.8 jars with 2.6.2
+if [ -d /usr/hd*/2.*/storm/lib/ ] && [ $(ls -la /usr/hd*/2.*/storm/lib/log4j*2.6.2.jar | wc -l) != 3 ]; then
+  echo "Updating storm jar in storm lib dir..."
+  mkdir ${demo_root}/oldjars
+  mv /usr/hd*/2.*/storm/lib/log4j*-2.8.jar ${demo_root}/oldjars
+  cp ${demo_root}/hdp/reference-apps/iot-trucking-app/trucking-data-simulator/target/log4j*-2.6.2.jar /usr/hd*/2.*/storm/lib/
+fi
 
 
 echo "Setup complete"
